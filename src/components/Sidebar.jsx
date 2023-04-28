a1bcf4d06fe6765a85d6390eb50499b81aaae3ba
Jean <jeanncavalcante@gmail.com>
2023-04-28 12:25:30 -0300

    corrigindo sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,13 @@
-import Image from "next/image";
 import Logo from "./ui/Logo";
-import Link from "next/link";
-import { menuItems } from "@/utils/menuItems";
-import { useRouter } from "next/router";
 import Menu from "./Menu";
 
 const Sidebar = () => {
-  const menu = menuItems;
-  const router = useRouter().pathname;
+
   return (
     <aside className="h-screen hidden lg:flex flex-col bg-zinc-900 w-1/4 max-w-[300px] items-center p-4">
       <div className="fixed">
-        <div>
+        <div className="mb-14">
           <Logo />
-        </div>
-
-        <div className="my-8 hidden md:flex">
-          <img
-            width={60}
-            height={60}
-            className="rounded-full "
-            src="https://avatars.githubusercontent.com/u/83246183?v=4"
-          />
         </div>
 
         <Menu />
