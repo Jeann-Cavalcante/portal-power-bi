e88a9903846b5551ba3b1d07392df243e9f7cd0d
Jean <jeanncavalcante@gmail.com>
2023-04-28 12:10:12 -0300

    correção da posição do link

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -8,17 +8,16 @@ const Menu = () => {
     <nav className="w-full">
       <ul className="flex flex-col gap-1 mt-4 text-zinc-400">
         {menuItems?.map((item) => (
-          <li
-            key={item.id}
-            className={`
+          <Link key={item.id} href={item.path}>
+            <li
+              className={`
               ${router === item.path ? "bg-zinc-700" : ""}
-              hover:bg-zinc-700 duration-300 rounded-md py-3 px-2`}
-          >
-            <Link className="flex gap-x-2 items-center" href={item.path}>
+              hover:bg-zinc-700 duration-300 rounded-md py-3 px-2 w-full flex gap-x-2 items-center`}
+            >
               {item.icon}
               <span className=" font-semibold">{item.name}</span>
-            </Link>
-          </li>
+            </li>
+          </Link>
         ))}
       </ul>
     </nav>
